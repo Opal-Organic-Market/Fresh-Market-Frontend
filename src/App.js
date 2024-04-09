bbff4aac44cb0117a43ea46ca3eb3a8afdc55a0f
JoanaQuadzie <joanaq03@mail.com>
2024-04-09 05:50:48 -0700

    created onboarding2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Onboarding1 from './pages/onboarding1';
+import Onboarding2 from './pages/onboarding2';
 
 
 export default function App() {
   return (
     <>
       <Onboarding1 />
+      <Onboarding2 />
     </>
   );
 }
